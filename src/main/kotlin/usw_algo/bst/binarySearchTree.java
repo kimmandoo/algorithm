4da818468760b718ab4319ffb23664c7d85e5d63
kimmandoo <mingyu5675@gmail.com>
2022-10-11 22:09:01 +0900

    bst done

--- src/main/kotlin/usw_algo/bst/binarySearchTree.java
+++ src/main/kotlin/usw_algo/bst/binarySearchTree.java
@@ -2,7 +2,6 @@ package usw_algo.bst;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class binarySearchTree {
     Node head = null;
@@ -27,7 +26,6 @@ public class binarySearchTree {
         for (int i : input) {
             bst.insert(i);
         }
-
 //        System.out.println(input);
     }
 
@@ -92,23 +90,92 @@ public class binarySearchTree {
                 return true;
             }
             // =======
-            while (delNode != null){
-                if (delNode.value == data){
+            while (delNode != null) {
+                if (delNode.value == data) {
                     searched = true;
                     break;
-                }else if (data < delNode.value){
+                } else if (data < delNode.value) {
                     pNode = delNode;
                     delNode = delNode.lNode;
-                }else{
+                } else {
                     pNode = delNode;
                     delNode = delNode.rNode;
                 }
             }
-            if (searched == false){
+            if (searched == false) {
                 return false;
             }
         }
-        //이제 삭제할 차례
+        // 이제 삭제할 차례
+        // 삭제할 노드가 leaf인 경우 -> 부모 브랜치를 끊어버림
+        if (delNode.lNode == null && delNode.rNode == null) {
+            if (data < delNode.value) {
+                pNode.lNode = null;
+                delNode = null;
+            } else {
+                pNode.rNode = null;
+                delNode = null;
+            }
+        } else if (delNode.lNode != null && delNode.rNode == null) {
+            // 삭제할 노드가 child 1개를 갖고있는 경우 -> 좌, 우 케이스 쪼개서 생각함
+            // 왼쪽에 child node
+            if (data < pNode.value) {
+                // child node의 방향은 고정
+                pNode.lNode = delNode.lNode;
+            } else {
+                pNode.rNode = delNode.lNode;
+            }
+        } else if (delNode.lNode == null && delNode.rNode != null) {
+            // 삭제할 노드가 child 1개를 갖고있는 경우 -> 좌, 우 케이스 쪼개서 생각함
+            if (data < pNode.value) {
+                pNode.lNode = delNode.rNode;
+            } else {
+                pNode.rNode = delNode.rNode;
+            }
+            return true;
+        } else if (delNode.lNode != null && delNode.rNode != null) {
+            // 삭제할 노드가 child를 2개 다 갖고있는 경우 -> 서브트리를 갖고있는 경우
+            if (data < pNode.value) {
+                // 삭제할 노드가 부모 왼쪽에 있을 때
+                Node tmpNode = delNode.rNode;
+                Node pTmpNode = delNode.rNode;
+                // 삭제할 노드의 오른쪽 자식 중, 가장 작은 노드 찾기
+                while (delNode.lNode != null) {
+                    pTmpNode = delNode;
+                    tmpNode = delNode.lNode;
+                }
+                // 여기까지 실행되면, tmpNode 에는 delNode의 오른쪽 자식의 서브트리 중 가장 작은값이 들어있음(왼쪽으로 계속 들어갔기 때문)
+                // 이제 tmpNode에 child가 붙어있을 경우를 생각해야됨.
+                if (tmpNode.rNode != null) {
+                    pTmpNode.lNode = tmpNode.rNode;
+                } else {
+                    pTmpNode.lNode = null;
+                }
+                pNode.lNode = tmpNode;
+                tmpNode.rNode = delNode.rNode;
+                tmpNode.lNode = delNode.lNode;
+                delNode = null;
+            } else {
+                // 삭제할 노드가 부모 오른쪽에 있을 때
+                // 삭제할 노드의 오른쪽 자식 중, 가장 작은 노드 찾기
+                Node tmpNode = delNode.rNode;
+                Node pTmpNode = delNode.rNode;
+                while (tmpNode.lNode != null) {
+                    pTmpNode = tmpNode;
+                    tmpNode = tmpNode.lNode;
+                }
+                if (tmpNode.rNode != null) {
+                    pTmpNode.lNode = tmpNode.rNode;
+                } else {
+                    pTmpNode.lNode = null;
+                }
 
+                pNode.rNode = tmpNode;
+                tmpNode.rNode = delNode.rNode;
+                tmpNode.lNode = delNode.lNode;
+            }
+            return true;
+        }
+        return false;
     }
 }
