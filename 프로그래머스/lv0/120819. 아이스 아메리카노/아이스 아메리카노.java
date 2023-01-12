c88d8b797a83fc8338d081ee8176cad40c140d04
Mingyu Kim <mingyu5675@gmail.com>
2023-01-12 18:10:57 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        int ame = 5500;
+        answer[0] = money / ame;
+        answer[1] = money - ame*answer[0];
+        return answer;
+    }
+}
