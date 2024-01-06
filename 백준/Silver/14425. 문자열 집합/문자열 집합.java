93d91725e0e8dc7c89c554c1d3be4f564e93e31c
Mingyu Kim <mingyu5675@gmail.com>
2024-01-06 21:50:22 +0900

    [Silver IV] Title: 문자열 집합, Time: 1076 ms, Memory: 69840 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,34 +1,30 @@
-import java.util.*;
+import java.io.FileInputStream;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
-class Main {
-  static int n;
-  static int m;
+class Main{
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    
-    n = sc.nextInt();
-    m = sc.nextInt();
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
-    ArrayList<String> S = new ArrayList<>(n);
-    ArrayList<String> testCase = new ArrayList<>(m);
-    
-    for(int i=0; i<n; i++){
-      S.add(sc.next());
+        Set<String> target = new HashSet<>();
+
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            target.add(sc.next());
+        }
+
+        for(int i=0; i<m; i++){
+            String input = sc.next();
+            if (target.contains(input)) ans++;
+        }
+
+
+        System.out.println(ans);
+
+        sc.close();
     }
-    
-    for(int i=0;i<m; i++){
-      testCase.add(sc.next());
-    }
-    System.out.print(Solution(S, testCase));
-
-  }
-
-  static int Solution(ArrayList<String> S, ArrayList<String> testCase){
-    int result = 0;
-    for(int i=0; i<m; i++){
-      if(S.contains(testCase.get(i))) result++;
-    }
-    return result;
-  }
 }
