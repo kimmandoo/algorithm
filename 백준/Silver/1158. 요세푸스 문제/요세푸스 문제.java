6be6bdb86217fc94baaabd53078f0cd186aaf05e
Mingyu Kim <mingyu5675@gmail.com>
2024-01-11 23:00:13 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 716 ms, Memory: 296324 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> q = new LinkedList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        for(int i=1; i<=n; i++){
+            q.offer(i); // 1부터 N까지
+        }
+        System.out.print("<");
+        while(!q.isEmpty()){
+            if(q.size()==1){
+                System.out.print(q.poll());
+            }else{
+                // k-1만큼 뒤로 보내고, poll
+                for(int i=0; i<k-1; i++){
+                    q.offer(q.poll());
+                }
+                System.out.print(q.poll()+", ");
+            }
+        }
+        System.out.print(">");
+    }
+}
