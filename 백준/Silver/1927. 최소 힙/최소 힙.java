660f1d39c5de81f2dd726c6ba5458f1e7ab9a4e7
Mingyu Kim <mingyu5675@gmail.com>
2024-03-10 23:23:50 +0900

    [Silver II] Title: 최소 힙, Time: 324 ms, Memory: 26080 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>((o1,o2)-> Integer.compare(o1, o2));
+		for(int i=0; i<n; i++) {
+			int tmp = Integer.parseInt(br.readLine());
+			if(tmp==0) {
+				if(pq.isEmpty()) {
+					sb.append("0").append("\n");
+				}else {
+					sb.append(pq.poll()).append("\n");
+				}
+			}else {
+				pq.add(tmp);
+			}
+		}
+		System.out.println(sb);
+	}
+}
