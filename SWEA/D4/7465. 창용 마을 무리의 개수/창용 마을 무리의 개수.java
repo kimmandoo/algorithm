6a71bda9e16c3fab9e0dc2f3cef08ab87820874d
Mingyu Kim <mingyu5675@gmail.com>
2024-03-21 09:39:53 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 129 ms, Memory: 24,952 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -2,50 +2,58 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-	public static void main(String[] args) throws Exception {
-		//System.setIn(new FileInputStream("res/input.txt"));
+	static int[] p;
+	static int n;
 
+	static void make() {
+		p = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			p[i] = i;
+		}
+	}
+
+	static int find(int a) {
+		if (p[a] == a)
+			return a;
+		return p[a] = find(p[a]);
+	}
+
+	static boolean union(int a, int b) {
+		int pa = find(a);
+		int pb = find(b);
+		if (pa == pb) {
+			return false;
+		}
+		p[pb] = pa;
+		return true;
+	}
+
+	public static void main(String[] args) throws Exception {
+		// System.setIn(new FileInputStream("res/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		StringBuilder sb = new StringBuilder();
 		int T;
 		T = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
 
 		for (int test_case = 1; test_case <= T; test_case++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			int n = Integer.parseInt(st.nextToken());
+			n = Integer.parseInt(st.nextToken());
 			int m = Integer.parseInt(st.nextToken());
-			int[][] map = new int[n + 1][n + 1]; // 1~n 명
-			boolean[] v = new boolean[n + 1]; // true가 되면 무리에 속했다는 말.
-			int muri = 0;
+			make();
+			int cnt = n;
 			for (int i = 0; i < m; i++) {
 				st = new StringTokenizer(br.readLine(), " ");
-				int start = Integer.parseInt(st.nextToken());
-				int end = Integer.parseInt(st.nextToken());
-				map[start][end] = 1;
-				map[end][start] = 1;
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				if(union(a, b)) cnt--;
 			}
+			
+//			System.out.println(Arrays.toString(p));
 
-			for (int i = 1; i <= n; i++) {
-				if (!v[i]) { // 무리 찾았다.
-					LinkedList<Integer> s = new LinkedList();
-					s.add(i);
-					v[i] = true;
-					while (!s.isEmpty()) {
-						int node = s.pollLast();
-						for (int k = 1; k <= n; k++) {
-							if (map[node][k] == 1 && !v[k]) {
-								s.add(k);
-								v[k] = true;
-							}
-						}
-					}
-					muri++;
-				}
-			}
-
-			sb.append("#").append(test_case).append(" ").append(muri).append("\n");
+			sb.append("#").append(test_case).append(" ").append(cnt).append("\n");
 		}
+
 		System.out.println(sb);
 	}
 }
