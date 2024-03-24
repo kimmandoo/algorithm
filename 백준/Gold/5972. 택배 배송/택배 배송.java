07a42b52afcc1b2f2622ba1c3012ca7983a49f5b
Mingyu Kim <mingyu5675@gmail.com>
2024-03-24 19:42:45 +0900

    [Gold V] Title: 택배 배송, Time: 452 ms, Memory: 40272 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static class Node {
+		int end, w;
+
+		Node(int end, int w) {
+			this.end = end;
+			this.w = w;
+		}
+	}
+
+	static int v, e;
+	static List<Node>[] list;
+	static int[] dist;
+	static PriorityQueue<Node> pq;
+
+	public static void main(String[] args) throws Exception {
+		// System.setIn(new FileInputStream("res/boj.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		v = Integer.parseInt(st.nextToken());
+		e = Integer.parseInt(st.nextToken());
+		int s = 1;
+		int INF = 100000000;
+		list = new ArrayList[v + 1]; // 각 정점 별로 인접리스트를 만든다.
+		dist = new int[v + 1]; // 가장 짧은 거리를 저장할 dist 배열
+
+		Arrays.fill(dist, INF); // 모든 정점의 값을 일단 INF로 초기화한다. min 값이 저장될 것이기 때문에
+		for (int i = 1; i <= v; i++) {
+			list[i] = new ArrayList<>(); // 각 노드(정점)에 연결된 리스트 생성
+		}
+
+		pq = new PriorityQueue<Node>((o1, o2) -> {
+			return o1.w - o2.w;
+		});
+
+		// 간선 개수
+		for (int i = 0; i < e; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			// from에서 to로 가는 weight 가중치
+			list[from].add(new Node(to, w));
+			list[to].add(new Node(from, w));
+		}
+		dijkstra(s);
+//		StringBuilder sb = new StringBuilder();
+//		for (int i = 1; i <= v; i++) {
+//			if (dist[i] == INF)
+//				sb.append("INF\n");
+//			else
+//				sb.append(dist[i]).append("\n");
+//		}
+
+		System.out.println(dist[v]);
+
+	}
+
+	private static void dijkstra(int start) {
+		boolean[] check = new boolean[v + 1]; // 확정된 노드를 표시함
+		pq.add(new Node(start, 0));
+		dist[start] = 0;
+
+		while (!pq.isEmpty()) {
+			Node curNode = pq.poll();
+			int cur = curNode.end;
+
+			if (check[cur] == true)
+				continue;
+			check[cur] = true;
+
+			for (Node node : list[cur]) {
+				if (dist[node.end] > dist[cur] + node.w) {
+					dist[node.end] = dist[cur] + node.w;
+					pq.add(new Node(node.end, dist[node.end]));
+				}
+			}
+		}
+	}
+}
