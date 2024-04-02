19b02adc636970b98f9e1a035e4de206e198ec4e
Mingyu Kim <mingyu5675@gmail.com>
2024-04-02 22:45:45 +0900

    [Gold V] Title: 평범한 배낭, Time: 196 ms, Memory: 53792 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -15,23 +15,19 @@ public class Main {
 		int[][] input = new int[n + 1][2]; // 각 물건의 0-무게, 1-가치
 		for (int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			// i번째 물건의 무게, 가치
 			int weight = Integer.parseInt(st.nextToken());
 			int value = Integer.parseInt(st.nextToken());
 			input[i][0] = weight;
 			input[i][1] = value;
 		}
 
-		// 평범한 배낭
 		for (int i = 1; i <= n; i++) {
-			// N개의 배낭
-			for (int w = 1; w <= k; w++) { // 최대 k 무게를 담을 때 때까지 반복한다
-				if (input[i][0] > w) { // 물건을 담을 수 없는 경우다
-					// 이 경우 그냥 이전 물건을 담을 상태를 넣어준다(물건 안 담을 거니까)
-					dp[i][w] = dp[i - 1][w]; // 무게가 들어가는게 아니라 가치가 들어간다
+			for (int w = 1; w <= k; w++) { // 최대 k 무게를 담을 때
+				if (input[i][0] > w) {
+					dp[i][w] = dp[i - 1][w];
 				}
-				else { // 물건을 담을 수 있는 경우다 
-					// 이전 물건을 담은 상태가 지금 물건을 담을 경우와 비교했을 때 더 크면, 물건을 안담고 넘어간다 
+				else {
+					// 이전 물건을 담은 상태가 더 크면, 물건을 안담고 넘어감
 					dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - input[i][0]] + input[i][1]);
 				}
 			}
