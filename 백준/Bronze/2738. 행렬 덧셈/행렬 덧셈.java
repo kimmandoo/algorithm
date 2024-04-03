20513de6c641d0f5ce9c3f84d4bf227b580d7977
Mingyu Kim <mingyu5675@gmail.com>
2024-04-03 23:17:10 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 656 ms, Memory: 36436 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,45 +1,30 @@
-import java.util.*;
+import java.util.Scanner;
 
-class Main {
-  static int n;
-  static int m;
-  
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    n = sc.nextInt();
-    m = sc.nextInt();
-    
-    int[][] arr1 = new int[n][m];
-    int[][] arr2 = new int[n][m];
-    //m1
-    for(int i=0; i<n; i++){
-      for(int j=0; j<m; j++){
-        arr1[i][j] = sc.nextInt();
-      }
-    }
-    //m2
-    for(int i=0; i<n; i++){
-      for(int j=0; j<m; j++){
-        arr2[i][j] = sc.nextInt();
-      }
-    }
-    sc.close();
-    int[][] result = Solution(arr1, arr2);
-    for(int i=0; i<n; i++){
-      for(int j=0; j<m; j++){
-        System.out.print(result[i][j]+" ");
-      }
-        System.out.print("\n");
-    }
-  }
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr1 = new int[n][m];
+        int[][] arr2 = new int[n][m];
 
-  static int[][] Solution(int[][] arr1, int[][] arr2){
-    int[][] result = new int[n][m];
-    for(int i=0; i<n; i++){
-      for(int j=0; j<m; j++){
-        result[i][j] = arr1[i][j] + arr2[i][j];
-      }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr1[i][j]+arr2[i][j]+" ");
+                if(j == m-1)
+                    System.out.println();
+            }
+        }
     }
-    return result;
-  }
 }
