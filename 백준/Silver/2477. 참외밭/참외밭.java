8c9bfc974f685d34a4de184d362ee366fc079c30
Mingyu Kim <mingyu5675@gmail.com>
2024-04-06 22:11:23 +0900

    [Silver II] Title: 참외밭, Time: 124 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Silver/2477. 참외밭/참외밭.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] agrs) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int sum = 0;
+		int max = Integer.MIN_VALUE;
+		int K = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		st.nextToken();
+		int first = Integer.parseInt(st.nextToken());
+		int pre = first;
+        
+		for (int i = 1; i < 6; i++) {
+			st = new StringTokenizer(br.readLine());
+			st.nextToken();
+			int cur = Integer.parseInt(st.nextToken());
+			max = Math.max(cur * pre, max);
+			sum += cur * pre;
+			pre = cur;
+		}
+		max = Math.max(pre * first, max);
+		sum += pre * first;
+		System.out.println((sum - max * 2)*K);
+	}
+}
