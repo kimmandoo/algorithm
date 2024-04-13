2fa5679dd6215bc0e0b695a13074aadbdf16c084
Mingyu Kim <mingyu5675@gmail.com>
2024-04-13 22:54:35 +0900

    [Silver III] Title: 계단 오르기, Time: 224 ms, Memory: 18092 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] stair = new int[301];
+        int[] dp = new int[301];
+        for (int i = 1; i <= N; i++)
+            stair[i] = sc.nextInt();
+
+        dp[1] = stair[1];
+        dp[2] = stair[1] + stair[2];
+        dp[3] = Math.max(stair[1], stair[2]) + stair[3];
+
+        for (int n = 4; n <= N; n++) {
+            dp[n] = Math.max(dp[n - 3] + stair[n - 1], dp[n - 2]) + stair[n];
+        }
+
+        System.out.println(dp[N]);
+    }
+}
