b02e8398c0bde225a902c7814d925f701ae3245d
Mingyu Kim <mingyu5675@gmail.com>
2024-09-05 23:28:00 +0900

    [Silver II] Title: 나무 자르기, Time: 976 ms, Memory: 119436 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -5,30 +5,32 @@ public class Main {
 	static int[] namu;
 	static int n;
 	static int c;
-
+	
+	// 작은 범위로 쪼개서 생각하는 습관을 기르자
 	static long count(int m) { // 자른 나무 개수 세기
-		long cnt = 0; 
-//		int h = namu[0]; // 순회 돌 준비 
-
-		for (int i = 0; i < n; i++) {
-			long d = namu[i] - m; // 나무 자르기
-			if (d > 0) {
+		// m은 중간나무 크기다.
+		long cnt = 0; // cnt가 int 범위를 넘어선다.
+		for (int i = 0; i < n; i++) { // n개의 나무를 다 돌면서(100만까지라 무난하다)
+		// 이분탐색 돌릴거라서 nlogn이다
+			long d = namu[i] - m; // m 기준으로 나무를 다 잘라본다.
+			if (d > 0) { // m 이상인거만 싹 자르는 것
 				cnt+=d;
 			}
 		}
-		return cnt;
+		return cnt; // m이상인 나무를 싹 잘라서 합친 수
 	}
 
 	static int parametric() {
 		int l = 0;
-		int r = namu[n-1];
+		int r = namu[n-1]; // 끝 원소인데, 오름차순 정렬했으니까 제일 큰 나무가 될 것
 		
 		int ans = 0;
 
 		while (l <= r) {
 			int m = (l + r) / 2;
 			long cnt = count(m); // 길이를 m만큼 자르면?
-			if (cnt >= c) { // 나무가 c랑 딱 맞다.
+			if (cnt >= c) { // 나무가 c랑 딱 맞다
+				// c보다 
 				ans = m;
 				l = m + 1;
 			} else { // 작으니까 H를 좀 줄이자.
@@ -48,14 +50,11 @@ public class Main {
 		c = Integer.parseInt(st.nextToken()); // 자른 나무 총 합
 
 		namu = new int[n];
-		// 그래프 초기화
 		st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < n; i++) {
 			namu[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(namu);
-//		System.out.println(Arrays.toString(namu));
-
+		Arrays.sort(namu); //이분탐색을 위해서 오름차순으로 정렬한다.
 		System.out.println(parametric());
 	}
 }
