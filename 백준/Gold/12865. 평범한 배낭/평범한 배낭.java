16e8b0db8e1b8300349fc6c777ed078d10eb5063
Mingyu Kim <mingyu5675@gmail.com>
2024-09-12 09:07:40 +0900

    [Gold V] Title: 평범한 배낭, Time: 168 ms, Memory: 53764 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,37 +1,41 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int n, k; // 물품수 N과 가방에 들어갈 수 있는 최대 무게 K
 
-	public static void main(String[] args) throws Exception {
-		// System.setIn(new FileInputStream("res/input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int n = Integer.parseInt(st.nextToken()); // 물품의 수
-		int k = Integer.parseInt(st.nextToken()); // 최대 무게
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("res/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        int[][] input = new int[n + 1][2]; // 각 물건의 무게, 가치
+        int[][] dp = new int[n + 1][k + 1]; // 최대 용량이 k인데, n개의 물건이 들어갈 지 말지 모름
 
-		int[][] dp = new int[n + 1][k + 1]; // 용량이 k인 배낭이고, 각 row의 인덱스에는 해당 용량일 때의 가치값이 들어가있다.
-		int[][] input = new int[n + 1][2]; // 각 물건의 0-무게, 1-가치
-		for (int i = 1; i <= n; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int weight = Integer.parseInt(st.nextToken());
-			int value = Integer.parseInt(st.nextToken());
-			input[i][0] = weight;
-			input[i][1] = value;
-		}
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            input[i][0] = w;
+            input[i][1] = v;
+        }
 
-		for (int i = 1; i <= n; i++) {
-			for (int w = 1; w <= k; w++) { // 최대 k 무게를 담을 때
-				if (input[i][0] > w) {
-					dp[i][w] = dp[i - 1][w];
-				}
-				else {
-					// 이전 물건을 담은 상태가 더 크면, 물건을 안담고 넘어감
-					dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - input[i][0]] + input[i][1]);
-				}
-			}
-		}
-		System.out.println(dp[n][k]);
-	}
+        // 이제 배낭 채우기
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                if (input[i][0] > j) {
+                    // 들어갈 물건의 무게갸 현재 j보다 크면 가방에 못넣음
+                    dp[i][j] = dp[i - 1][j]; // 최대 가치 넣기
+                } else { // 가방에 넣을 수 있으면
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - input[i][0]] + input[i][1]);
+                    // dp배열에는 가지가 들어가는데, 이전 가치랑, 현재 물건의 가치+ 이전 물건 가치
+                }
+            }
+        }
+        System.out.println(dp[n][k]);
+    }
+
 }
