4fddd23ebbb6358e006f635fae00074a5325281c
Mingyu Kim <mingyu5675@gmail.com>
2024-09-26 23:36:03 +0900

    [Silver III] Title: 수열, Time: 264 ms, Memory: 26464 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,27 +1,39 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-class Main {
+public class Main {
+    static int n, gap;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("res/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        gap = Integer.parseInt(st.nextToken());
+        int[] in = new int[n];
+        int[] prefix = new int[n+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            in[i] = Integer.parseInt(st.nextToken());
+            prefix[i+1] = prefix[i] + in[i];
+        }
+//        System.out.println(Arrays.toString(prefix));
 
-		int n = sc.nextInt();
-		int k = sc.nextInt();
-		int[] sy = new int[n];
-		List<Integer> l = new ArrayList<>();
-		for (int i = 0; i < n; i++) {
-			sy[i] = sc.nextInt();
-		}
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+            return o2 - o1;
+        });
+        //이제 각 간격의 합을 pq에 넣기
+        int idx = n;
+        while (true) {
+            if (idx - gap < 0) break;
+            pq.add(prefix[idx] - prefix[idx - gap]);
+            idx--;
+        }
 
-		for (int i = 0; i <= n - k; i++) {
-			int tmp = 0;
-			for (int j = i; j < i + k; j++) {
-				tmp += sy[j];
-			}
-			l.add(tmp);
-		}
-		Collections.sort(l, null);
-		System.out.println(l.get(l.size() - 1));
-		sc.close();
-	}
+        System.out.println(pq.poll());
+    }
 }
