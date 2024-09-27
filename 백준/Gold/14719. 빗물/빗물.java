a730ffed271115b1948cf93680cd8cbbd7ee8350
Mingyu Kim <mingyu5675@gmail.com>
2024-09-27 23:03:30 +0900

    [Gold V] Title: 빗물, Time: 100 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -1,71 +1,48 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int n, m;
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("res/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int H = Integer.parseInt(st.nextToken()); // 세로 크기
+        int W = Integer.parseInt(st.nextToken()); // 가로 크기
 
-	static class Bitmool {
-		int idx, h;
+        int[] heights = new int[W]; // 블록의 높이 배열
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < W; i++) {
+            heights[i] = Integer.parseInt(st.nextToken());
+        }
 
-		Bitmool(int idx, int h) {
-			this.idx = idx;
-			this.h = h;
-		}
-	}
+        int[] leftMax = new int[W];
+        int[] rightMax = new int[W];
 
-	public static void main(String[] args) throws Exception {
-		// System.setIn(new FileInputStream("res/input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		Deque<Bitmool> s = new ArrayDeque();
-		st = new StringTokenizer(br.readLine(), " ");
-		int sum = 0;
-		int[] res = new int[m];
-		int[] block = new int[m];
-		// 일단 넣고, 큰거 나오면 pop, 작은거나오면 차이만큼 더하기
-		for (int i = 0; i < m; i++) {
-			block[i] = Integer.parseInt(st.nextToken());
-			if (s.isEmpty()) {
-				s.add(new Bitmool(i, block[i]));
-			} else {
-				Bitmool tmp = s.peekFirst();
-				if (tmp.h > block[i]) {
-					s.add(new Bitmool(i, block[i]));
-				} else {
-					// 다 빼고 하나 넣음
-					while (!s.isEmpty()) {
-						Bitmool stacked = s.pollFirst();
-						res[stacked.idx] = tmp.h - stacked.h;
-					}
-					s.add(new Bitmool(i, block[i]));
-				}
-			}
-		}
-		s.clear();
-		for (int i = m - 1; i > 0; i--) {
-			if (s.isEmpty()) {
-				s.add(new Bitmool(i, block[i]));
-			} else {
-				Bitmool tmp = s.peekFirst();
-				if (tmp.h > block[i]) {
-					s.add(new Bitmool(i, block[i]));
-				} else {
-					// 다 빼고 하나 넣음
-					while (!s.isEmpty()) {
-						Bitmool stacked = s.pollFirst();
-						if (res[stacked.idx] == 0)
-							res[stacked.idx] = tmp.h - stacked.h;
-					}
-					s.add(new Bitmool(i, block[i])); 
-				}
-			}
-		}
+        // 왼쪽에서 본 최대 높이 계산
+        leftMax[0] = heights[0];
+        for (int i = 1; i < W; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], heights[i]);
+        }
 
-		for (int i : res) {
-			sum += i;
-		}
-		System.out.println(sum);
-	}
+        // 오른쪽에서 본 최대 높이 계산
+        rightMax[W - 1] = heights[W - 1];
+        for (int i = W - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], heights[i]);
+        }
+
+        // 빗물이 고일 수 있는 양 계산
+        int water = 0;
+        for (int i = 0; i < W; i++) {
+            // 고일 수 있는 물의 양은 좌우에서 본 최대 높이 중 더 작은 값에서 현재 기둥의 높이를 뺀 값
+            int minHeight = Math.min(leftMax[i], rightMax[i]);
+            if (minHeight > heights[i]) {
+                water += minHeight - heights[i];
+            }
+        }
+
+        System.out.println(water);
+    }
 }
