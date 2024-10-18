d883604272490cb558e3863d21992fe3c4cdf7fc
Mingyu Kim <mingyu5675@gmail.com>
2024-10-19 00:24:14 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 128 ms, Memory: 14872 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,37 +1,37 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    public static int lis(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        // 각 위치에서 수열길이 1로 채워넣고 시작
-        Arrays.fill(dp, 1);
-        int len = 1;
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
-            len = Math.max(len, dp[i]);
-        }
-
-        return len;
-    }
-
     public static void main(String[] args) throws Exception {
-        // System.setIn(new FileInputStream("res/boj.txt"));
+        // System.setIn(new FileInputStream("res/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-        int size = Integer.parseInt(br.readLine());
-        int[] input = new int[size];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[] dp = new int[n+1]; // 맨처음도 이전과 비교할 수 있도록 +1개 더 많이 해준다.
         st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < size; i++) {
+        for(int i=0; i<=n; i++){
+            // 일단 다 1로 초기화
+            Arrays.fill(dp,1);
+        }
+        int prevMax = 1; // 왼쪽에 존재하는 애들 중에 가장 큰 거
+        int[] input = new int[n+1];
+        for(int i=1; i<=n; i++){
             input[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(lis(input));
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<i; j++){ // 내 기준 왼쪽에 있는 숫자까지 확인할 것
+                if(input[i] > input[j]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+        }
+        int max = 0;
+        for(int i=0; i<=n; i++){
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
     }
 }
