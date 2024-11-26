178bd689cd60e26fc6160e8097ab58ca5e70d79b
Mingyu Kim <mingyu5675@gmail.com>
2024-11-26 15:36:42 +0900

    [Silver III] Title: N과 M (2), Time: 108 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,48 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
 public class Main {
-	static int n, m;
-	static int[] num;
-	static int[] result;
-	static boolean[] v;
+    static int n, m;
+    static StringBuilder sb;
 
-	public static void pmt(int depth, int start) {
-		if (depth == m + 1) {
-			// 오름차순 순열만 필터링
-			StringBuilder sb = new StringBuilder();
-			for (int i = 1; i <= m; i++) {
-				sb.append(result[i]).append(" ");
-			}
-			System.out.println(sb);
-			return;
-		}
-		for (int i = start; i <= n; i++) {
-			if (!v[i]) {
-				v[i] = true;
-				result[depth] = num[i];
-				pmt(depth + 1, i + 1);
-				v[i] = false;
-			}
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("res/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        sb = new StringBuilder();
+        arr = new LinkedList<>();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        // n까지의 자연수로 길이가 m인 수열 만들기
+        go(0);
+        System.out.println(sb);
+    }
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		m = sc.nextInt();
-		num = new int[n + 1];
-		result = new int[m + 1];
-		v = new boolean[n + 1];
-		// n개 중 m개를 고른 수열, 수열의 출력은 사전순으로.
-		for(int i=1; i<=n; i++){
-            num[i] = i;
+    static LinkedList<Integer> arr;
+    public static void go(int k) {
+        if(k == m){
+            // sb에 추가하기
+            for(int i=0; i<k;i++){
+                sb.append(arr.get(i)).append(" ");
+            }
+            sb.append("\n");
+            return;
         }
-		Arrays.sort(num);
-		// 재귀
-		// pmt(1);
-		// DFS로 풀기(백트래킹)
-//		dfs(1,1);
-		pmt(1, 1);
-	}
+
+        for(int i=1; i<=n; i++){
+            // 중복을 제거한다
+            if(!arr.isEmpty() && arr.peekLast() >= i) continue;
+            arr.addLast(i);
+            go(k+1); // k는 자리수를 의미함
+            arr.removeLast();
+        }
+    }
 }
