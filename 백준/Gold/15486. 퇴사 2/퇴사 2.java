bf38ff962c34c088b29ed021d0ca81d39f540305
Mingyu Kim <mingyu5675@gmail.com>
2024-11-28 01:43:21 +0900

    [Gold V] Title: 퇴사 2, Time: 768 ms, Memory: 339708 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] input;
+    static int n;
+    static int[] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        input = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            input[i][0] = Integer.parseInt(st.nextToken());
+            input[i][1] = Integer.parseInt(st.nextToken());
+        }
+        
+        dp = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            int next = i + input[i][0];
+            if (next > n) {
+                dp[i] = dp[i + 1];
+            } else if (next == n) {
+                dp[i] = Math.max(input[i][1], dp[i + 1]);
+            } else {
+                dp[i] = Math.max(input[i][1] + dp[next], dp[i + 1]);
+            }
+        }
+        System.out.println(dp[0]);
+    }
+}
