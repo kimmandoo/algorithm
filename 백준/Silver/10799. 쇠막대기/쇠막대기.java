b3ed5c4283e3250d35bf62fd86e4e1629c6c49cf
Mingyu Kim <mingyu5675@gmail.com>
2024-12-05 13:05:36 +0900

    [Silver II] Title: 쇠막대기, Time: 160 ms, Memory: 16760 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -2,24 +2,30 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        char[] input = sc.next().toCharArray();
-        int res = 0;
-        Stack<Character> mongdungee = new Stack<>();
-        for(int i=0; i<input.length; i++){
-            if(i<input.length-1&& input[i] == '(' && input[i+1]==')'){
-                // 레이져 -> 레이져 이전에 있었던 거 더해주기
-                res += mongdungee.size();
-                i++; // 레이져 발사
+    static LinkedList<Character> st;
 
-            }else if(input[i] == '(') {
-                mongdungee.push(input[i]);
-            }else if(input[i] == ')'){
-                mongdungee.pop();
-                res++;
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new FileInputStream("res/boj.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        System.out.println(go(st.nextToken()));
+    }
+
+    public static int go(String input) {
+        st = new LinkedList<>();
+        int cnt = 0;
+        for (int i = 0; i < input.length(); i++) {
+            // 레이저 기준으로 다 더하면 된다 그랬는데 기억이 안나서 무식하게 풀었음
+            if (i < input.length() - 1 && input.charAt(i) == '(' && input.charAt(i + 1) == ')') {
+                cnt += st.size();
+                i++;
+            } else if (input.charAt(i) == '(') {
+                st.push(input.charAt(i));
+            } else if (input.charAt(i) == ')') {
+                st.pop();
+                cnt++;
             }
         }
-        System.out.println(res);
+        return cnt;
     }
 }
