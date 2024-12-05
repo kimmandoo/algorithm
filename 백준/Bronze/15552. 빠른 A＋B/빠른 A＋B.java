169148f1ef0923e08b4b6c47e33a7bc3659dae03
Mingyu Kim <mingyu5675@gmail.com>
2024-12-05 12:31:33 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 880 ms, Memory: 290212 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,12 +1,12 @@
 import java.io.*;
 
 class Main{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int size = Integer.parseInt(br.readLine());
-        for(int i=0; i<size; i++){
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++){
             String[] input = br.readLine().split(" ");
             int a = Integer.parseInt(input[0]);
             int b = Integer.parseInt(input[1]);
