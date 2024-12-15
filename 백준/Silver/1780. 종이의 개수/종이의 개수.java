9f7b61af59b336dcbecd51055b3146053245356f
Mingyu Kim <mingyu5675@gmail.com>
2024-12-15 17:32:19 +0900

    [Silver II] Title: 종이의 개수, Time: 1020 ms, Memory: 314456 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -1,69 +1,75 @@
 import java.util.*;
 import java.io.*;
 
-class Main {
-	static int mod;
-	static int[][] map;
-	static int[] res;
+public class Main {
+    static int n;
+    static int[][] b;
+    static int zero = 0;
+    static int minus = 0;
+    static int plus = 0;
 
-	public static void main(String[] args) throws Exception {
-		//System.setIn(new FileInputStream("res/input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int n = Integer.parseInt(st.nextToken());
-		map = new int[n][n];
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < n; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		res = new int[3]; // -1, 0, 1 개수 넣기
-		go(0, 0, n);
-		for(int i: res) {
-			System.out.println(i);
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("res/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        b = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < n; j++) {
+                b[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        // 입력 끝
+        go(0,0,n);
+        System.out.println(minus);
+        System.out.println(zero);
+        System.out.println(plus);
+    }
 
-	public static void go(int ci, int cj, int n) {
-		if (n == 1) {
-			if (map[ci][cj] == -1) {
-				res[0]++;
-			} else if (map[ci][cj] == 0) {
-				res[1]++;
-			} else {
-				res[2]++;
-			}
-			return;
-		}
-		boolean check = true;
-		int base = map[ci][cj];
-		a: for (int i = ci; i < ci + n; i++) {
-			for (int j = cj; j < cj + n; j++) {
-				if (base != map[i][j]) {
-					check = false;
-					break a;
-				}
-			}
-		}
-		if (check) {
-			if (map[ci][cj] == -1) {
-				res[0]++;
-			} else if (map[ci][cj] == 0) {
-				res[1]++;
-			} else {
-				res[2]++;
-			}
-			return;
-		}
-		go(ci, cj, n / 3);
-		go(ci, cj + n / 3, n / 3);
-		go(ci, cj + 2 * n / 3, n / 3); // 위에 세줄
-		go(ci + n / 3, cj, n / 3);
-		go(ci + n / 3, cj + n / 3, n / 3);
-		go(ci + n / 3, cj + 2 * n / 3, n / 3); // 가운데 세줄
-		go(ci + 2 * n / 3, cj, n / 3);
-		go(ci + 2 * n / 3, cj + n / 3, n / 3);
-		go(ci + 2 * n / 3, cj + 2 * n / 3, n / 3);
-	}
+    public static void go(int ci, int cj, int size) {
+        // 종이가 모두 같은 수로 되어있으면 그만
+        // 그게 아니면 9등분
+        // 그리고 그거 반복
+        boolean allSame = true;
+        int tmp = -2;
+        a:
+        for (int i = ci; i < ci + size; i++) {
+            for (int j = cj; j < cj + size; j++) {
+                if (tmp == -2) tmp = b[i][j];
+                else {
+                    if (tmp != b[i][j]) {
+                        allSame = false;
+                        break a;
+                    }
+                }
+            }
+        }
+        if (!allSame) {
+            // 위에 세칸
+            go(ci, cj, size / 3);
+            go(ci, cj + size / 3, size / 3);
+            go(ci, cj + (size * 2) / 3, size / 3);
+            // 중안 세칸
+            go(ci + size / 3, cj, size / 3);
+            go(ci + size / 3, cj + size / 3, size / 3);
+            go(ci + size / 3, cj + (size * 2) / 3, size / 3);
+            // 아래 세칸
+            go(ci + (size * 2) / 3, cj, size / 3);
+            go(ci + (size * 2) / 3, cj + size / 3, size / 3);
+            go(ci + (size * 2) / 3, cj + (size * 2) / 3, size / 3);
+        } else {
+            switch (tmp) {
+                case 0:
+                    zero++;
+                    return;
+                case -1:
+                    minus++;
+                    return;
+                case 1:
+                    plus++;
+                    return;
+            }
+        }
+    }
 }
