570a6a6d270046786b1526cd3139de443d06f44c
Mingyu Kim <mingyu5675@gmail.com>
2024-12-15 18:13:42 +0900

    [Silver I] Title: RGB거리, Time: 108 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,40 +1,37 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+    static int n;
+    static int[][] dp;
 
-	public static void main(String[] args) throws Exception {
-		// System.setIn(new FileInputStream("res/boj.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null; // new StringTokenizer(br.readLine(), " ");
-		int n = Integer.parseInt(br.readLine());
-		int[][] in = new int[n+1][3];
-		for (int i = 1; i <= n; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			in[i][0] = Integer.parseInt(st.nextToken());
-			in[i][1] = Integer.parseInt(st.nextToken());
-			in[i][2] = Integer.parseInt(st.nextToken());
-		}
-		// 입력 끝
-		int[][] dp = new int[n+1][3];
-		dp[1][0] = in[1][0];
-		dp[1][1] = in[1][1];
-		dp[1][2] = in[1][2];
-
-		for (int i = 2; i <= n; i++) {
-			dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + in[i][0];
-			dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + in[i][1];
-			dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][0]) + in[i][2];
-		}
-		
-		int min = Integer.MAX_VALUE;
-		for(int i=0; i<3; i++) {
-			min = Math.min(min, dp[n][i]);
-		}
-		System.out.println(min);
-
-	}
-
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("res/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        dp = new int[n + 1][3];
+        // dp에 저장되는 값은 항상 최소값이어야함
+        for (int i = 0; i < 3; i++) {
+            Arrays.fill(dp[i], 1001 * 1001);
+        }
+        dp[0][0] = 0;
+        dp[0][1] = 0;
+        dp[0][2] = 0;
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int r = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            // 맞닿은 집이랑 비교하면 안됨
+            dp[i][0] = r + Math.min(dp[i - 1][1], dp[i - 1][2]);
+            dp[i][1] = g + Math.min(dp[i - 1][0], dp[i - 1][2]);
+            dp[i][2] = b + Math.min(dp[i - 1][1], dp[i - 1][0]);
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < 3; i++) {
+            min = Math.min(min, dp[n][i]);
+        }
+        System.out.println(min);
+    }
 }
