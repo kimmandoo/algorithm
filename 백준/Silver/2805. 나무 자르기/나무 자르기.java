6266fdda466ece5ba65c432c1e9da7ca4fb1a705
Mingyu Kim <mingyu5675@gmail.com>
2024-12-19 17:14:42 +0900

    [Silver II] Title: 나무 자르기, Time: 888 ms, Memory: 119532 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,60 +1,48 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-	static int[] namu;
-	static int n;
-	static int c;
-	
-	// 작은 범위로 쪼개서 생각하는 습관을 기르자
-	static long count(int m) { // 자른 나무 개수 세기
-		// m은 중간나무 크기다.
-		long cnt = 0; // cnt가 int 범위를 넘어선다.
-		for (int i = 0; i < n; i++) { // n개의 나무를 다 돌면서(100만까지라 무난하다)
-		// 이분탐색 돌릴거라서 nlogn이다
-			long d = namu[i] - m; // m 기준으로 나무를 다 잘라본다.
-			if (d > 0) { // m 이상인거만 싹 자르는 것
-				cnt+=d;
-			}
-		}
-		return cnt; // m이상인 나무를 싹 잘라서 합친 수
-	}
+    static int n, m;
+    static int[] tree;
 
-	static int parametric() {
-		int l = 0;
-		int r = namu[n-1]; // 끝 원소인데, 오름차순 정렬했으니까 제일 큰 나무가 될 것
-		
-		int ans = 0;
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("res/boj.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken()); // 나무, 집으로 가져가려는 나무 길이 M
+        tree = new int[n];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            tree[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(tree);
+        go();
+    }
 
-		while (l <= r) {
-			int m = (l + r) / 2;
-			long cnt = count(m); // 길이를 m만큼 자르면?
-			if (cnt >= c) { // 나무가 c랑 딱 맞다
-				// c보다 
-				ans = m;
-				l = m + 1;
-			} else { // 작으니까 H를 좀 줄이자.
-				r = m - 1;
-			}
-		}
+    public static void go() {
+        int l = 0;
+        int r = tree[n - 1];
+        while (l <= r) {
+            int mid = (l + r) / 2; // mid는 나무 길이를 의미한다.
+            if (bulmok(mid)) { // 조건을 만족한 경우에는 값을 좀 나무 높이를 더 올릴 수 있나 봐야됨
+                l = mid + 1;
+            } else {
+                r = mid - 1; // 조건 만족 못하면 내려야됨
+            }
+        }
+        System.out.println(r);
+    }
 
-		return ans;
-	}
-
-	public static void main(String[] args) throws Exception {
-		//System.setIn(new FileInputStream("res/input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-		n = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken()); // 자른 나무 총 합
-
-		namu = new int[n];
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < n; i++) {
-			namu[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(namu); //이분탐색을 위해서 오름차순으로 정렬한다.
-		System.out.println(parametric());
-	}
+    public static boolean bulmok(int mid) {
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (tree[i] > mid) {
+                sum += tree[i] - mid;
+            }
+        }
+        return sum >= m;
+    }
 }
