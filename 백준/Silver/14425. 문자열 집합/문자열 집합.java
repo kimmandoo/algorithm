d2f0314e3a306163a2e8574927c6b5a61baed036
Mingyu Kim <mingyu5675@gmail.com>
2024-12-24 12:16:05 +0900

    [Silver IV] Title: 문자열 집합, Time: 960 ms, Memory: 68944 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -4,27 +4,20 @@ import java.util.Scanner;
 import java.util.Set;
 
 class Main{
-
     public static void main(String[] args) throws Exception{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-
         Set<String> target = new HashSet<>();
-
         int ans = 0;
         for(int i=0; i<n; i++){
             target.add(sc.next());
         }
-
         for(int i=0; i<m; i++){
             String input = sc.next();
             if (target.contains(input)) ans++;
         }
-
-
         System.out.println(ans);
-
         sc.close();
     }
 }
