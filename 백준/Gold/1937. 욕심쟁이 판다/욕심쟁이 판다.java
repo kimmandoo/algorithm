24a385ff2404cd634068389a68626e9ee49cd1be
Mingyu Kim <mingyu5675@gmail.com>
2025-03-11 13:41:04 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 480 ms, Memory: 37248 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,52 +1,66 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
-	static int[] di = { -1, 0, 1, 0 };
-	static int[] dj = { 0, 1, 0, -1 };
+public class Main {
 	static int n;
 	static int[][] map;
 	static int[][] dp;
-	static int max = -1;
+	static int[] di = { -1, 0, 1, 0 };
+	static int[] dj = { 0, 1, 0, -1 }; // 4방
 
-	public static int dfs(int ci, int cj) {
-		if (dp[ci][cj] != -1)
-			return dp[ci][cj]; // 방문했으니까 계산하지않고 반환한다.
-		dp[ci][cj] = 1; // 방문처리
-		for (int i = 0; i < 4; i++) {
-	        int ni = di[i] + ci;
-	        int nj = dj[i] + cj;
-	        if (isValid(ci, cj, ni, nj)) {
-	            dp[ci][cj] = Math.max(dp[ci][cj], 1 + dfs(ni, nj)); // 탐색을 진행하고 최댓값을 저장한다.
-	        }
-	    }
-	    return dp[ci][cj];
-	}
-
-	public static boolean isValid(int ci, int cj, int ni, int nj) {
-		return ni < n && nj < n && ni >= 0 && nj >= 0 && map[ci][cj] < map[ni][nj];
-	}
-
-	public static void main(String args[]) throws Exception {
+	public static void main(String[] args) throws Exception {
 		// System.setIn(new FileInputStream("res/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringTokenizer st = null;
+		st = new StringTokenizer(br.readLine(), " ");
 		n = Integer.parseInt(st.nextToken());
 		map = new int[n][n];
 		dp = new int[n][n];
+		for (int[] r : dp) {
+			Arrays.fill(r, -1); // 방문안한걸 -1로 하기로 함
+		}
+		int max = -1;
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			for (int j = 0; j < n; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				dp[i][j] = -1;
 			}
 		}
+		// 입력 받기 끝
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				dfs(i, j);
+				go(i,j);
 				max = Math.max(max, dp[i][j]);
 			}
 		}
+		
 		System.out.println(max);
+		
+//		for(int[] r: dp) {
+//			System.out.println(Arrays.toString(r));
+//		}
+	}
+
+	public static int go(int ci, int cj) {
+		if (dp[ci][cj] != -1)
+			return dp[ci][cj]; // 이미 더 긴 값이 존재하면 끊어버리기
+
+		dp[ci][cj] = 1;
+		
+		for (int i = 0; i < 4; i++) {
+			int ni = ci + di[i];
+			int nj = cj + dj[i];
+			if (isValid(ni, nj) && map[ni][nj] > map[ci][cj]) {
+				// 유효한 길이고 대나무가 더 많으면
+				dp[ci][cj] = Math.max(dp[ci][cj], go(ni, nj) + 1);
+			}
+		}
+
+		return dp[ci][cj];
+	}
+
+	public static boolean isValid(int ci, int cj) {
+		// 갈 수 있는지
+		return ci < n && cj < n && ci >= 0 && cj >= 0;
 	}
 }
