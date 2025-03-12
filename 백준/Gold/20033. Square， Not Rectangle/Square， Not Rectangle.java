08f59b8cdf26f1fa17a0af4b0bd0bbc2ae2d52b7
Mingyu Kim <mingyu5675@gmail.com>
2025-03-13 00:49:41 +0900

    [Gold IV] Title: Square, Not Rectangle, Time: 392 ms, Memory: 46240 KB -BaekjoonHub

---
+++ 백준/Gold/20033. Square， Not Rectangle/Square， Not Rectangle.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] input;
+    static int low, high;
+
+    public static void main(String[] args) throws IOException {
+        // System.setIn(new FileInputStream("res/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        input = new int[n];
+        for (int i = 0; i < n; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+            high = Math.max(high, input[i]);
+            low = Math.min(low, input[i]);
+        }
+        // k * k 가 최대가 되는 시점이 필요함 
+        go();
+    }
+
+    public static void go() {
+        int l = low;
+        int r = high;
+        int res = 0;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (check(m)) {
+                res = m;
+                l = m + 1; // 정사각형을 만들 수 있으면 더 크게 만들기
+            } else {
+                r = m - 1;
+            }
+        }
+        System.out.println(res);
+    }
+
+    public static boolean check(int size) {
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (cnt >= size) return true;
+            if (input[i] >= size){
+                cnt++;
+            }else{
+                // 이게 아니면
+                cnt = 0;
+            }
+        }
+
+        return cnt >= size;
+    }
+}
