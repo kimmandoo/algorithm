721ecc9efd476b77e2153952e5dfbfaf38ce6941
Mingyu Kim <mingyu5675@gmail.com>
2025-05-17 23:28:37 +0900

    [Silver II] Title: 에디터, Time: 60 ms, Memory: 21020 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -4,11 +4,10 @@
 
 using namespace std;
 
-void out(list<char> ll){
+void out(auto& ll){
     for(auto c: ll){
         cout << c;
     }
-    cout << "\n";
 }
 
 int main(){
@@ -16,10 +15,10 @@ int main(){
     string input;
     cin >> input;
     list<char> ll;
-    list<char>::iterator t = ll.begin();
     for(auto c: input){
-        ll.insert(t, c);
+        ll.push_back(c);
     }
+    auto t = ll.end();
     
     int tc = 0; 
     cin >> tc;
@@ -27,22 +26,23 @@ int main(){
         char cmd; 
         char in;
         cin >> cmd;
-        if(cmd == 'P'){
-            cin >> in;
-            ll.insert(t, in);
-        }
-        if(cmd == 'L'){
-            if(t != ll.begin()) t--;
-        }
-        if(cmd == 'D'){
-            if(t != ll.end()) t++;
-        }
         if(cmd == 'B'){
             if(t != ll.begin()){ // 맨 앞이 아닐 때만
                 t--; // 하나 뒤로 돌린다음 삭제해야됨
                 t = ll.erase(t);
                 // t가 가리키는 위치를 지우고 그 다음위치를 반환함
             }
+        }
+        else if(cmd == 'L'){
+            if(t != ll.begin()) t--;
+        }
+        else if(cmd == 'D'){
+            if(t != ll.end()) t++;
+        }
+        else{
+            cin >> in;
+            ll.insert(t, in);
+            
         }
     }
     out(ll);
