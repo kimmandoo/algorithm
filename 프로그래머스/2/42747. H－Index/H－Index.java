9a2f2371676a683dac8c954fc101709ed7d996cd
Mingyu Kim <mingyu5675@gmail.com>
2025-07-06 20:24:47 +0900

    [level 2] Title: H-Index, Time: 0.67 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,20 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
+        int n = citations.length;
         Arrays.sort(citations);
-        int len = citations.length;
-        for(int i=0; i<len; i++){
-            int tmpH = citations[i];
-            // 현재 위치 포함해서 남은 논문 편 수
-            int left = len - i;
-            if(tmpH <= left){
-                answer = tmpH;
+        // int h = 0;
+        // for(int i=1; i<=n; i++){
+        //     // i번째 논문의 인용 수 체크 1-based
+        //     int cit = citations[i-1];
+        //     // 인용된 논문이 h편 이상인가?
+        //     if(n-i+1 >= h){
+        //         // 남은 논문 개수가 현재 인용 수 보다 크거나 같으면 성립
+        //         h = cit;
+        //     }
+        // }
+        
+        for (int i = 0; i < n; i++) {
+            int h = n - i; // 남은 논문 개수
+            if (citations[i] >= h) {
+                return h;
             }
         }
         
-        
-            
-        return answer;
+        return 0;
     }
 }
