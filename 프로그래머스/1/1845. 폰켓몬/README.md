1d08ffac9576f48a844ca76d11fbe555344653ad
Mingyu Kim <mingyu5675@gmail.com>
2025-07-06 21:22:24 +0900

    [level 1] Title: 폰켓몬, Time: 4.46 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 100 MB, 시간: 7.19 ms
+메모리: 95.4 MB, 시간: 4.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 25일 15:44:22
+2025년 07월 06일 21:22:23
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,16 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        TreeSet<Integer> ts = new TreeSet();
+        HashSet<Integer> p = new HashSet();
         for(int i: nums){
-            ts.add(i); // 일단 이러면 중복제거가 된다
+            p.add(i);
         }
-        if(ts.size() >= nums.length/2){
-            answer = nums.length/2;
-        }else{
-            answer = ts.size();
-        }
+        int pick = nums.length / 2;
+        if(pick >= p.size()) return p.size();
         
-        return answer;
+        return pick;
     }
 }
