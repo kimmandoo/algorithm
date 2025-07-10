5bd66ba040dc729a34b5e9226667d7fc1d33df27
Mingyu Kim <mingyu5675@gmail.com>
2025-07-10 20:30:35 +0900

    [level 1] Title: 최소직사각형, Time: 1.60 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -3,14 +3,12 @@ import java.util.*;
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
-        int w = 0; // max가 될 것
-        int h = 0; // min이 될 것
+        int w = 0;
+        int h = 0;
         for(int[] m: sizes){
             int tw = m[0];
             int th = m[1];
-            // 가로 세로 해두고, 가로가 작다면 돌려서 가로가 더 크게 만든다
             if(tw < th){
-                // 가로가 더 작아서 회전시킴
                 w = Math.max(th, w);
                 h = Math.max(tw, h);
             }else{
