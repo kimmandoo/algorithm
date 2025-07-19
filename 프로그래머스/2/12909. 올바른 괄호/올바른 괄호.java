3bde46faa07ec083bf5817c3190d6f31d7dac07d
Mingyu Kim <mingyu5675@gmail.com>
2025-07-19 14:25:09 +0900

    [level 2] Title: 올바른 괄호, Time: 14.12 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,24 +2,21 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        LinkedList<Character> stack = new LinkedList();
-        for(int i=0; i<s.length(); i++){
-            char tmp = s.charAt(i);
-            if(stack.isEmpty()){
-                if(tmp == ')') return false; 
-                stack.add(tmp);
-            }
-            else if(stack.peekLast() != tmp){
-                stack.pollLast();
-            }else{
-                stack.add(tmp);
+        LinkedList<Character> st = new LinkedList<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c == '(') {
+                st.addLast(c);
+            } else { // c == ')'
+                if (st.isEmpty()) {
+                    return false;
+                }
+                st.removeLast(); // 짝이 맞으면 pop
             }
         }
-        if(stack.size() != 0){
-            answer = false;
-        }
-
-        return answer;
+        
+        return st.isEmpty(); // 다 짝 맞으면 비어 있어야 함
     }
 }
