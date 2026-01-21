79ab2d9e678770d3ff8caa7523c67899da5127c3
Mingyu Kim <mingyu5675@gmail.com>
2026-01-21 23:21:51 +0900

    [Bronze V] Title: We love kriii, Time: 84 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static char[][] map;
+    static int si, sj, fi, fj;
+    static int[] di = {-1, 0, 1, 0};
+    static int[] dj = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("res/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//        n = Integer.parseInt(st.nextToken());
+//        m = Integer.parseInt(st.nextToken());
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+
+    public static void go(){
+
+    }
+}
