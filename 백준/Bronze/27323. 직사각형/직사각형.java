68de032f1962164460a79404e813f47394474a7c
Mingyu Kim <mingyu5675@gmail.com>
2026-01-28 21:31:57 +0900

    [Bronze V] Title: 직사각형, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/27323. 직사각형/직사각형.java
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -1,11 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-
+    static int n, x;
+    static int[] input;
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("res/boj.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
         System.out.println(a*b);
     }
 }
