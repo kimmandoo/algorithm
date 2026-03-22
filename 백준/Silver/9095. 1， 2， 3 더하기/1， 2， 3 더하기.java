e797883eb739e589fc0b32458f133a9e73c84753
Mingyu Kim <mingyu5675@gmail.com>
2026-03-22 17:25:17 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,22 +1,44 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] dp;
+    static int n;
+    static int mx = -1;
+    static int[] memo;
+
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        dp = new int[11];
-        int n = sc.nextInt();
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
-        for(int i=4; i<=10; i++){
-            dp[i] = dp[i-1]+dp[i-2]+dp[i-3]; // 점화식
+        // System.setIn(new FileInputStream("res/boj.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        int[] input = new int[n];
+        for (int i = 0; i < n; i++) {
+            int target = Integer.parseInt(br.readLine());
+            input[i] = target;
+            mx = Math.max(mx, target);
         }
-        for(int i=0; i<n; i++){
-            int input = sc.nextInt();
-            System.out.println(dp[input]);
+
+        memo = new int[mx + 1];
+        Arrays.fill(memo, -1);
+
+        // 최대값까지 한 번 계산
+        go(mx);
+
+        StringBuilder sb = new StringBuilder();
+        for (int e : input) {
+            sb.append(go(e)).append('\n');
         }
+        System.out.print(sb);
+    }
+
+    static int go(int x) {
+        if (x == 0) return 1;
+        if (x < 0) return 0;
+
+        if (memo[x] != -1) return memo[x];
+
+        memo[x] = (go(x - 1) + go(x - 2) + go(x - 3)) % 1_000_000_009;
+        return memo[x] % 1_000_000_009;
     }
 }
-
